c6baea6bc03a15f3f3503f9c3c1390be320584e9
jacobkdunn <134105105+jacobkdunn@users.noreply.github.com>
2023-05-19 16:02:28 -0600

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Jacob Dunn on 2023-05-19.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
